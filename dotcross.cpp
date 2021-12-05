3349327db8d8e207dd5d214265be498bdf20fb70
eeypt4 <eeypt4@nottingham.ac.uk>
2021-12-05 20:38:56 +0000

    Update vector 3D-Vector class

---
+++ dotcross.cpp
@@ -0,0 +1,31 @@
+////Sample
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    float a1, a2, a3, b1, b2, b3, c1, c2, c3, d, addX, addY, addZ, subX, subY, subZ;
+//    cout << "Enter components of first vector:\n";
+//    cin >> a1 >> a2 >> a3;
+//    cout << "Enter components of second vector:\n";
+//    cin >> b1 >> b2 >> b3;
+//
+//    addX = a1+b1;
+//    addY = a2+b2;
+//    addZ = a3+b3;
+//    cout << "\nAddition =" << "<" << addX << ", " << addY << ", " << addZ << ">";
+//
+//    subX = a1-b1;
+//    subY = a2-b2;
+//    subZ = a3-b3;
+//    cout << "\nSubtraction =" << "<" << subX << ", " << subY << ", " << subZ << ">";
+//
+//    c1 = a2*b3-a3*b2;
+//    c2 = a3*b1-a1*b3;
+//    c3 = a1*b2-a2*b1;
+//    cout << "\nCross Product =" << c1 << "i+ " << c2 << "j+ " << c3 << "k";
+//
+//    d = a1*b1 + a2*b2 + a3*b3;
+//    cout << "\nDot Product = " << d;
+//}
---
+++ vector.cpp
@@ -0,0 +1,50 @@
+////Sample
+//
+//#include <iostream>
+//
+//class vector3d {
+//private:
+//    int vectorID;
+//    float x;
+//    float y;
+//    float z;
+//
+//public:
+//    //constructor
+//    vector3d(int vectorID, float x, float y, float z);
+//
+//    //destructor
+//    ~vector3d();
+//};
+//
+//vector3d :: vector3d(int vectorID, float x, float y, float z){
+//    this->x = x;
+//    this->y = y;
+//    this->z = z;
+//}
+//
+//
+//
+//        set x, y, z
+//        get_x()
+//        get_y()
+//        get_z()
+//        get_vectorID()
+//
+//
+//        //accessor
+//
+//        //mutator
+//
+//        addition()
+//        getSubtraction()
+//        getScalarProduct()
+//        getVectorProduct()
+//        // Store vectors
+//        // Addition: a+b = <x1+x2, y1+y2, z1+z2>
+//        // Subtraction: a-b = <x1-x2, y1-y2, z1-z2>
+//        // Scalar Product: a.b = x1x2 + y1y2 + z1z2
+//        // Vector Product: axb = <>
+//
+//int main() {
+//}
