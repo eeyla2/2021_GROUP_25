cdd9ffec095c0ff6cd572102c0a3a38e1a9dfce4
Azim Adebisi <azimadebisi9@gmail.com>
2021-12-14 15:27:03 +0000

    intial commit material class

---
+++ Material.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class material{
+private:
+    int materialIndex;
+    float density;
+    string materialName;
+    string colour;
+
+public:
+    material(int materialIndex, float density, string colour, string materialName);
+    ~material();
+
+    int getMaterialIndex();
+    float getDensity();
+    string getMaterialName();
+    string colour();
+
+    void setMaterialIndex( int i);
+    void setDensity(float d);
+    void setMaterialName(string n);
+    void setColour(string c);
+  };
+
