649567102e9716411966695655e1b7868e7d1b71
Daniel-Ischebeck <eeydi1@nottingham.ac.uk>
2022-01-07 12:58:00 +0000

    matrix files

---
+++ src/matrix.cpp
@@ -0,0 +1,9 @@
+//Matrix.cpp - Best
+
+//Some comments / thoughts
+
+// You will want constructors for different standard matricies 
+// some examples are where diagonals are 1, rest 0, (gets used in some operations)
+// all zeros etc.
+
+//how do we want to store data in matrix, array, std::vector (be careful to avoid confusion with Vector3d)
---
+++ src/matrix.hpp
@@ -0,0 +1,1 @@
+//Matrix header - Best
